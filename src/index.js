702ad12ad270b616d4c2afbba4745ade5dc84b9b
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-06-25 16:31:03 +0900

    fixed google analytics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import GA4React from "ga-4-react";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+try {
+  setTimeout((_) => {
+    const ga4react = new GA4React("G-TFRNKYL96D");
+    ga4react.initialize().catch((err) => console.error(err));
+  }, 4000);
+} catch (err) {
+  console.error(err);
+}
